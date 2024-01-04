4896acd8afd65529a65b552f37fc880f2b1720c9
naveen kumar <naveenkumarlanka77@gmail.com>
2024-01-04 10:28:12 +0530

    book store

--- src/components/Sidebar/SideBar.jsx
+++ src/components/Sidebar/SideBar.jsx
@@ -1,11 +1,10 @@
 import React, { useContext, useState } from "react";
-
 import { AuthContext } from "../../Context/ReactContext";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
-import "../SideBar/SideBar.css";
 import { Sidebar } from "flowbite-react";
 import { FaHouseChimneyUser } from "react-icons/fa6";
 import { BiBuoy } from "react-icons/bi";
+import "../../components/SideBar/SideBar.css";
 
 import {
   HiArrowSmRight,
